ca1d1ff8ad2ce8df57c293600931df47dad93b78
Briza5 <BrizaL555@gmail.com>
2025-11-17 20:27:27 +0100

    add backend file with api calling

---
+++ backend.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+
+def get_data(place, forecast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+if __name__ == "__main__":
+    get_data(place="Tokyo")
